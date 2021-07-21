15b33ccafd862e1befbecd1311426742c0294919
tgfive <67025703+tgfive@users.noreply.github.com>
2021-07-20 19:39:17 -0500

    Added comments

--- local-model/odefcn.m
+++ local-model/odefcn.m
@@ -1,5 +1,14 @@
-function dydt = odefcn(y,b,g)
-  %sir system of ode's
+function dydt = odefcn(y,p)
+  %ODEFCN   SIR system of ode's.
+  %   dydt = odefcn(y,p) where
+  %   y(1) = S(t), y(2) = I(t),
+  %   p(1) = beta, p(2) = gamma
+  
+  %initialize dydt array
   dydt = zeros(2,1);
-  dydt(1) = - b * y(1) * y(2);
-  dydt(2) = b * y(1) * y(2) - g * y(2);
+  
+  %dydt(1) = dSdt
+  dydt(1) = - p(1) * y(1) * y(2);
+  %dydt(2) = dIdt
+  dydt(2) = p(1) * y(1) * y(2) - p(2) * y(2);
+end
