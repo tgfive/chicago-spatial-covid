b74a8263ccf8239ea6f6a3d07a111681689fab91
tgfive <67025703+tgfive@users.noreply.github.com>
2021-10-27 18:46:21 -0500

    Covid progression graph with sources

--- data/data_collection.py
+++ data/data_collection.py
@@ -34,10 +34,33 @@ df = df.loc["2020-03-01":"2021-10-26"]
 # Export to a csv
 df.to_csv("chicago_data.csv")
 
+# Create labels
+label_list = [
+    (pd.to_datetime("2020-03-21"), 'Shelter-in-place\nbegins.', 'm', 1000),
+    (pd.to_datetime("2020-04-24"), 'Mask mandate\nbegins.', 'brown', 1550),
+    (pd.to_datetime("2020-05-5"), 'Phase 2', 'c', 200),
+    (pd.to_datetime("2020-05-29"), 'Shelter-in-place\nends.', 'm', 1000),
+    (pd.to_datetime("2020-06-3"), 'Phase 3', 'c', 550),
+    (pd.to_datetime("2020-06-26"), 'Phase 4', 'c', 200),
+    (pd.to_datetime("2021-01-25"), 'Limited vaccination begins.', 'indigo', 1000),
+    (pd.to_datetime("2021-04-12"), 'General vaccination\nbegins.', 'indigo', 500),
+    (pd.to_datetime("2021-05-18"), 'Mask mandate\nends.', 'brown', 1050),
+    (pd.to_datetime("2021-06-11"), 'Phase 5', 'c', 200),
+    (pd.to_datetime("2021-08-30"), 'Mask mandate\nreinstated.', 'brown', 1000)
+]
+
 # Create plot
 df.plot(figsize=(12,8))
+
+ax = plt.gca()
+for date_point, label, clr, y in label_list:
+    plt.axvline(x=date_point, color=clr)
+    plt.text(date_point, ax.get_ylim()[1]-y, label,
+             horizontalalignment='center',
+             verticalalignment='center',
+             color=clr,
+             bbox=dict(facecolor='white', alpha=0.9))
 plt.show()
 
 # Export plot
-import tikzplotlib
-tikzplotlib.save("chicago-data.tex")
+plt.savefig('chicago-data.pdf')
